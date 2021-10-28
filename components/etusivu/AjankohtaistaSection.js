a9c41314d4578b57fea0d40a6bb1545f1f64a8bb
tranhuyviet <vietth@msn.com>
2021-10-29 01:04:20 +0300

    mobile version - ajankohtaista section

--- components/etusivu/AjankohtaistaSection.js
+++ components/etusivu/AjankohtaistaSection.js
@@ -6,11 +6,11 @@ const AjankohtaistaSection = () => {
         <section className='sectionWrap  bg-gray-50'>
             <h1 className='sectionTitle'>Ajankohtaista</h1>
             <div className='mt-10 relative max-w-4xl mx-auto'>
-                <div className='border w-full grid grid-cols-2 shadow-lg bg-primary-700'>
+                <div className='border w-full shadow-lg bg-primary-700 flex flex-col md:flex-row'>
                     <img
                         src='/images/rosen.jpg'
                         alt='exu'
-                        className='w-[420px] border-r'
+                        className='md:w-[480px]'
                     />
 
                     <div className='text-gray-500 py-6 px-6 flex items-center '>
@@ -18,16 +18,22 @@ const AjankohtaistaSection = () => {
                             <h2 className='text-2xl font-title font-semibold text-gray-100'>
                                 Rosen kehoterapia (ROSEN METHOD) 50e/h
                             </h2>
-                            <p className='mb-10 text-lg mt-6 text-gray-100'>
+                            <p className='mb-6 md:mb-10 text-lg mt-4 md:mt-6 text-gray-100'>
                                 Tuula Määttä Harjoitusjaksolla oleva
                                 Rosenterapia opiskelija p.050-4944652
                             </p>
-                            <Link href='https://slotti.fi/booking/hyvinvointi-jakauneushoitolaluminella/'>
-                                <a className='btn-reverse'>Netti Ajanvaraus</a>
-                            </Link>
-                            <Link href='https://www.facebook.com/Rosenterapeuttifi-342042090069211'>
-                                <a className='btn-reverse ml-4'>Facebook</a>
-                            </Link>
+                            <div className='flex flex-col justify-center sm:items-center space-x-0 space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4'>
+                                <Link href='https://slotti.fi/booking/hyvinvointi-jakauneushoitolaluminella/'>
+                                    <a className='btn-reverse'>
+                                        Netti Ajanvaraus
+                                    </a>
+                                </Link>
+                                <Link href='https://www.facebook.com/Rosenterapeuttifi-342042090069211'>
+                                    <a className='btn-reverse ml-4'>
+                                        Rosen Facebook
+                                    </a>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
             <Hero />
             <TeamSection />
             <GiftSection />
-            {/* <AjankohtaistaSection />  */}
+            <AjankohtaistaSection />
         </div>
     );
 }
