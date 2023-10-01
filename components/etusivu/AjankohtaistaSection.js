b14fbe05f5fdecac410f086644eabd45e7c2a45d
Viet Tran <viet.tran@qoco.fi>
2023-10-01 17:39:40 +0300

    update home page

--- components/etusivu/AjankohtaistaSection.js
+++ components/etusivu/AjankohtaistaSection.js
@@ -1,40 +1,33 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 const AjankohtaistaSection = () => {
-    return (
-        <section className='sectionWrap  bg-gray-50'>
-            <h1 className='sectionTitle'>Ajankohtaista</h1>
-            <div className='mt-10 relative max-w-4xl mx-auto'>
-                <div className='border w-full shadow-lg bg-primary-700 flex flex-col md:flex-row'>
-                    <img
-                        src='/images/rosen.jpg'
-                        alt='exu'
-                        className='md:w-[480px]'
-                    />
+  return (
+    <section className="sectionWrap  bg-gray-50">
+      <h1 className="sectionTitle">Ajankohtaista</h1>
+      <div className="mt-10 relative max-w-4xl mx-auto">
+        <div className="border w-full shadow-lg bg-primary-700 flex flex-col md:flex-row">
+          <img src="/images/rosen.jpg" alt="exu" className="md:w-[480px]" />
 
-                    <div className='text-gray-500 py-6 px-6 flex items-center '>
-                        <div>
-                            <h2 className='text-2xl font-title font-semibold text-gray-100'>
-                                Rosen kehoterapia (ROSEN METHOD) 50e/h
-                            </h2>
-                            <p className='mb-6 md:mb-10 text-lg mt-4 md:mt-6 text-gray-100'>
-                                Tuula Määttä Harjoitusjaksolla oleva
-                                Rosenterapia opiskelija p.050-4944652
-                            </p>
-                            <div className='flex flex-col justify-center sm:items-center space-x-0 space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4'>
-                                <Link href='https://slotti.fi/booking/hyvinvointi-jakauneushoitolaluminella/'>
-                                    <a className='btn-reverse'>
-                                        Netti Ajanvaraus
-                                    </a>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          <div className="text-gray-500 py-6 px-6 flex items-center ">
+            <div>
+              <h2 className="text-2xl font-title font-semibold text-gray-100">
+                Rosen kehoterapia (ROSEN METHOD) 75e/h
+              </h2>
+              <p className="mb-6 md:mb-10 text-lg mt-4 md:mt-6 text-gray-100">
+                Tuula Määttä p.050-4944652
+              </p>
+              <div className="flex flex-col justify-center sm:items-center space-x-0 space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
+                <Link href="https://slotti.fi/booking/hyvinvointi-jakauneushoitolaluminella/">
+                  <a className="btn-reverse">Netti Ajanvaraus</a>
+                </Link>
+              </div>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AjankohtaistaSection;
--- components/etusivu/TeamSection.js
+++ components/etusivu/TeamSection.js
@@ -1,62 +1,62 @@
-import React from 'react';
-import TeamCard from './TeamCard';
+import React from "react";
+import TeamCard from "./TeamCard";
 
 const TeamSection = () => {
-    return (
-        <section className='sectionWrap relative overflow-hidden bg-gray-50'>
-            <h1 className='sectionTitle'>Luminellan ammattilaiset</h1>
-            <div className='flex flex-col px-8 justify-center items-center md:flex-row md:space-y-0 md:space-x-8 mt-8 space-y-8 space-x-0 md:mt-12'>
-                <TeamCard
-                    imgUrl='/images/Tuula.jpg'
-                    name='TUULA MÄÄTTÄ'
-                    text1='Diplomi-Kosmetologi'
-                    text2='Rosen kehoterapia'
-                    text3='Hieronnan AMT'
-                    phone='050-494 4652'
-                />
+  return (
+    <section className="sectionWrap relative overflow-hidden bg-gray-50">
+      <h1 className="sectionTitle">Luminellan ammattilaiset</h1>
+      <div className="flex flex-col px-8 justify-center items-center md:flex-row md:space-y-0 md:space-x-8 mt-8 space-y-8 space-x-0 md:mt-12">
+        <TeamCard
+          imgUrl="/images/Tuula.jpg"
+          name="TUULA MÄÄTTÄ"
+          text1="Diplomi-Kosmetologi"
+          text2="Rosen kehoterapia"
+          text3="Hieronnan AMT"
+          phone="050-494 4652"
+        />
 
-                <TeamCard
-                    imgUrl='/images/Chrisu.jpg'
-                    name='CHRISTINA HOKKANEN'
-                    text1='Jalkojenhoitajan AMT'
-                    text2='Koulutettu hieroja'
-                    text3='Vyöhyketerapeutti'
-                    phone='040-414 0214'
-                />
+        <TeamCard
+          imgUrl="/images/Chrisu.jpg"
+          name="CHRISTINA HOKKANEN"
+          text1="Jalkojenhoitajan AMT"
+          text2="Perinteinen jäsenkorjaaja"
+          text3="Koulutettu hieroja"
+          phone="040-414 0214"
+        />
 
-                <TeamCard
-                    imgUrl='/images/Katja.jpg'
-                    name='KATJA LOUHISTO'
-                    text1='SKY-Kosmetologi'
-                    text2='Koulutettu hieroja'
-                    text3='Urheiluhieroja'
-                    phone='044-977 9505'
-                />
-            </div>
-            {/* <div className='absolute z-mandala opacity-30 top-0 right-0 translate-x-1/2 -translate-y-1/2'>
+        <TeamCard
+          imgUrl="/images/Katja.jpg"
+          name="KATJA LOUHISTO"
+          text1="SKY-Kosmetologi"
+          text2="Koulutettu hieroja"
+          text3="Urheiluhieroja"
+          phone="044-977 9505"
+        />
+      </div>
+      {/* <div className='absolute z-mandala opacity-30 top-0 right-0 translate-x-1/2 -translate-y-1/2'>
                 <img
                     src='/images/Mandala.svg'
                     alt='mandala'
                     className='h-[700px] w-[700px]'
                 />
             </div> */}
-            {/* <div className='absolute z-10 opacity-50 bottom-0 -translate-x-1/2'>
+      {/* <div className='absolute z-10 opacity-50 bottom-0 -translate-x-1/2'>
                 <img
                     src='/images/mandala-3.svg'
                     alt='mandala'
                     className='h-[540px] w-[540px] '
                 />
             </div> */}
-            <div className='absolute z-10  bottom-0 translate-y-1/2 w-full '>
-                <div className='relative'>
-                    <img
-                        src='/images/mandala-2.svg'
-                        alt='mandala'
-                        className='w-[80%] sm:w-[40%] absolute bottom-0 left-0 translate-y-1/2 opacity-[0.35] -translate-x-1/2'
-                    />
-                </div>
-            </div>
-            {/* <div className='absolute z-10  top-0 right-0 w-full bg-red-500'>
+      <div className="absolute z-10  bottom-0 translate-y-1/2 w-full ">
+        <div className="relative">
+          <img
+            src="/images/mandala-2.svg"
+            alt="mandala"
+            className="w-[80%] sm:w-[40%] absolute bottom-0 left-0 translate-y-1/2 opacity-[0.35] -translate-x-1/2"
+          />
+        </div>
+      </div>
+      {/* <div className='absolute z-10  top-0 right-0 w-full bg-red-500'>
                 <div className='relative bg-gray-500'>
                     <img
                         src='/images/mandala-3.svg'
@@ -65,17 +65,17 @@ const TeamSection = () => {
                     />
                 </div>
             </div> */}
-            <div className='absolute z-10  top-0 -translate-y-1/2 w-full'>
-                <div className='relative'>
-                    <img
-                        src='/images/mandala-1.svg'
-                        alt='mandala'
-                        className='w-[80%] sm:w-[40%]  opacity-[0.35] right-10 top-10 absolute translate-x-1/2 -translate-y-1/2'
-                    />
-                </div>
-            </div>
-        </section>
-    );
+      <div className="absolute z-10  top-0 -translate-y-1/2 w-full">
+        <div className="relative">
+          <img
+            src="/images/mandala-1.svg"
+            alt="mandala"
+            className="w-[80%] sm:w-[40%]  opacity-[0.35] right-10 top-10 absolute translate-x-1/2 -translate-y-1/2"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TeamSection;
